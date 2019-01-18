9c2f47a1e368425c71d96aa805ccd98ce3cfbfde
Zachary Mikel <zachary.mikel@gmail.com>
2019-01-17 23:37:10 -0700

    Added BST question

--- c#/golden/library/Thing.cs
+++ c#/golden/library/Thing.cs
@@ -17,7 +17,7 @@ namespace library
             var aChars = new List<char>(a.ToCharArray());
             var bChars = new LinkedList<char>(b.ToCharArray());
 
-            for(var i = 0; i < aChars.Length; i++) {
+            for(var i = 0; i < aChars.Count; i++) {
                 var bNode = bChars.Find(aChars[i]);
                 if(bNode != null) 
                 {
@@ -30,6 +30,68 @@ namespace library
             }
 
             return bChars.Count == 0; 
+        }
+    }
+
+
+    public class BST {
+
+        class Node {
+            public Node left {get; set;}
+            public Node right {get; set;}
+            public int value {get; set;}
+
+            public Node(int value) {
+                this.value = value;
+            }
+        }
+
+        Node root {get; set;}
+
+        public BST() {}
+
+        public BST(int rootValue) {
+            this.root = new Node(rootValue);
+        }
+
+        public void Add(int value) {
+            
+            if(this.root == null)
+                this.root = new Node(value);
+            
+            else {
+                AddNode(root, value);
+            }
+        
+        }
+
+        private void AddNode(Node root, int value) {
+            
+            if(value > root.value && root.right == null)
+            {
+                root.right = new Node(value);
+            }
+
+            else if(value < root.value && root.left == null)
+            {
+                root.left = new Node(value);
+            }
+
+            else 
+            {
+                if(value > root.right.value)
+                {
+                    AddNode(root.right, value);
+                }
+                else
+                {
+                    AddNode(root.left, value);
+                }
+            }
+        }
+
+        public void PrintTree() {
+            Console.WriteLine($"{this.root.value},{this.root.left.value},{this.root.right.value}");
         }
     }
 }
--- c#/golden/test-library/UnitTest1.cs
+++ c#/golden/test-library/UnitTest1.cs
@@ -6,10 +6,18 @@ namespace test_library
 {
     public class UnitTest1
     {
-        [Fact]
-        public void TestAnagram() {
-            Assert.True(new Thing().areAnagrams("cinema", "iceman"));
-        }
+        // [Fact]
+        // public void TestAnagram() {
+        //     Assert.True(new Thing().areAnagrams("cinema", "iceman"));
+        // }
         
+        [Fact]
+        public void TestBinarySearchTree() {
+            var tree = new BST(10);
+            tree.Add(5);
+            tree.Add(11);
+            tree.PrintTree();
+            Assert.True(tree != null);
+        }
     }
 }
